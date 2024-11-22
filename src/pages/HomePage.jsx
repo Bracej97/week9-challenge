8da575323ef872c5338d6690a8bd780daa06d702
Joe Bracey <joebracey1@gmail.com>
2024-11-22 22:46:35 +0000

    Photo added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,16 @@
 // HomePage.js
 import React from 'react';
+import picture from '../assets/IMG_7635.jpeg';
 
 const HomePage = () => {
   return (
     <div>
       <h2>Joe Bracey Coding</h2>
-      <p>This is the main content of the home page.</p>
+      <p>I am currently completing the Full Stack Web Development Skills Bootcamp with Step8Up. So far we have learnt HTML, JavaScript, Python, Django and React.</p>
+      <p />
+      <p>Outside of the bootcamp I am a Product Manager. I regularly attend the gym and play for the local cricket club.</p>
+      <h4>Photo of me</h4>
+      <img src={picture} style={{ width: "15rem"}}/>
     </div>
   );
 };
